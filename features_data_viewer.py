697f859b0cdf2cf0408618a69abb0c8fd5d29e82
Andrew Gilbride <108138271+agilbride2010@users.noreply.github.com>
2022-12-15 18:03:15 -0500

    added features data viewer and results formater

---
+++ features_data_viewer.py
@@ -0,0 +1,13 @@
+
+import sample_data as sample_data
+import numpy
+
+feature='SFOC'
+
+features_dataset=sample_data.sample_data()
+timestamps=features_dataset['timestamps']
+features_data=features_dataset['means'][feature]
+
+features_dt=numpy.vstack((timestamps,features_data))
+
+print(features_dt)
---
+++ results_format.py
@@ -0,0 +1,10 @@
+
+def results_formating(results_table , file_name):
+
+
+    if fileopen == 1
+        results_status='Successfull'
+        else
+        results_status='Fail'
+
+    return results_status
